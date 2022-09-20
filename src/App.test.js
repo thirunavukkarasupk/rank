0714cb688dfc7d69eb88a9e2c7286c7883e47fad
KumarThirunavukkarasu <thirunavukkarasupk@gmail.com>
2022-09-20 15:28:05 +0530

    TestCompletedForApp.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,48 @@
 import { render } from '@testing-library/react';
-
+import React from 'react';
 import App from './App';
 import context from './core/context';
+import * as Name from './components/NameInput';
+import * as MarksInput from './components/MarksInput';
+import * as SaveButton from './components/SaveButton';
+import * as StudentTable from './components/StudentTable';
+import * as Select from './components/Select';
+import * as FilteredTable from './components/FilteredTable';
 
 describe('App', () => {
 	test('renders the component appropriately', () => {
-		const component = render(App(context)).getByRole('App');
+		jest.spyOn(Name, 'default').mockReturnValue(<div role="name"/>);
+		jest.spyOn(MarksInput, 'default')
+			.mockReturnValue(<div role="marksInput"/>);
+		jest.spyOn(SaveButton, 'default')
+			.mockReturnValue(<div role="saveButton"/>);
+		jest.spyOn(StudentTable, 'default')
+			.mockReturnValue(<div role="studentTable"/>);
+		jest.spyOn(Select, 'default')
+			.mockReturnValue(<div role="select"/>);
+		jest.spyOn(FilteredTable, 'default')
+			.mockReturnValue(<div role="filteredTable"/>);
 
-		expect(component).toBeInTheDocument();
+		const { getByRole } = render(App(context));
+
+		expect(getByRole('App')).toBeInTheDocument();
+
+		expect(getByRole('name')).toBeInTheDocument();
+		expect(Name.default).toHaveBeenCalledWith(context, {});
+
+		expect(getByRole('marksInput')).toBeInTheDocument();
+		expect(MarksInput.default).toHaveBeenCalledWith(context, {});
+
+		expect(getByRole('saveButton')).toBeInTheDocument();
+		expect(SaveButton.default).toHaveBeenCalledWith(context, {});
+
+		expect(getByRole('studentTable')).toBeInTheDocument();
+		expect(StudentTable.default).toHaveBeenCalledWith(context, {});
+
+		expect(getByRole('select')).toBeInTheDocument();
+		expect(Select.default).toHaveBeenCalledWith(context, {});
+
+		expect(getByRole('filteredTable')).toBeInTheDocument();
+		expect(FilteredTable.default).toHaveBeenCalledWith(context, {});
 	});
 });
